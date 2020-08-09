202af6986c17bb6ceb592bd06ca8f75ca7dbd344
djking0 <48757105+djking0@users.noreply.github.com>
2020-08-09 23:24:50 +0800

    update ai counting and update time
    code.php infected

--- code.php
+++ code.php
@@ -19,25 +19,51 @@ $get_upload_data = "select * from old_upload where email='$insert_email'";
 $run_upload_data = mysqli_query($con, $get_upload_data);
 $row_upload_data = mysqli_fetch_array($run_upload_data);
 
+//User profile fetch
 $id = $row_user_data['id'];
 $username = $row_user_data['username'];
 $image = $row_user_data['image'];
 $email = $row_user_data['email'];
 
+//Upload history fetch
+$upload_id = $row_upload_data['id'];
+$upload_email = $row_upload_data['email'];
+$upload_count = $row_upload_data['count'];
+$upload_latest_time = $row_upload_data['time'];
+
+
+
 
 	//Update history downloaded data if existed email
 	if(mysqli_num_rows($run_user_data) > 0) {
-		$upload_query = "
-		INSERT INTO old_upload(
-		email,
-		time
-		) VALUES(
-		'$insert_email',
-		NOW()
-		)
-		";
-		$run_upload_query = mysqli_query($con, $upload_query);
-		echo mysqli_error($con);
+		if(mysqli_num_rows($run_upload_data) == 0 ){
+			$upload_query = "
+			INSERT INTO old_upload(
+			email,
+			count,
+			time
+			) VALUES(
+			'$insert_email',
+			'1',
+			NOW()
+			)
+			";
+			
+			$run_upload_query = mysqli_query($con, $upload_query);
+			echo mysqli_error($con);
+			echo "insert upload record";
+				
+		}else{
+			$upload_update_query = "
+			UPDATE old_upload SET
+			count = '$upload_count' + 1 ,
+			time = NOW()
+			WHERE email = '$insert_email'
+			";
+			echo "update upload record";
+			$upload_update_query_run = mysqli_query($con, $upload_update_query);
+			echo mysqli_error($con);
+			}
 	
 	//No email exist then insert to database 
 	}elseif(mysqli_num_rows($run_user_data) == 0) {
@@ -54,21 +80,12 @@ $email = $row_user_data['email'];
 		NOW()
 		)
 		";
-		$upload_query = "
-		INSERT INTO old_upload(
-		email,
-		time
-		) VALUES(
-		'$insert_email',
-		NOW()
-		)
-		";
+	
 		$insert_query_run = mysqli_query($con, $insert_query);
-		$run_upload_query = mysqli_query($con, $upload_query);
+		echo "user added to database";
 		echo mysqli_error($con);
 	}
 	
-
 }
 
 // download to database
@@ -77,7 +94,7 @@ $download = $_POST['is_download'];
 $insert_username = $_POST['username'];
 $insert_image = $_POST['image'];
 $insert_email = $_POST['email'];
-$insert_upload = $upload;
+$insert_download = $download;
 
 //fetch data and check for user profile
 $get_user_data = "select * from user where email='$insert_email'";
@@ -99,18 +116,34 @@ $email = $row_user_data['email'];
 
 	//Update history downloaded data if exist email
 	if(mysqli_num_rows($run_user_data) > 0) {
-		//Update history downloaded data
-	$download_query = "
-		INSERT INTO old_download(
-		email,
-		time
-		) VALUES(
-		'$insert_email',
-		NOW()
-		)
-		";
-	$run_download_query = mysqli_query($con, $download_query);
-	echo mysqli_error($con);
+		if(mysqli_num_rows($run_upload_data) == 0 ){
+			$download_query = "
+			INSERT INTO old_download(
+			email,
+			count,
+			time
+			) VALUES(
+			'$insert_email',
+			'1',
+			NOW()
+			)
+			";
+			
+			$run_download_query = mysqli_query($con, $download_query);
+			echo mysqli_error($con);
+			echo "inserted download record";
+				
+		}else{
+			$download_update_query = "
+			UPDATE old_upload SET
+			count = '$download_count' + 1 ,
+			time = NOW()
+			WHERE email = '$insert_email'
+			";
+			echo "updated download record";
+			$download_update_query_run = mysqli_query($con, $download_update_query);
+			echo mysqli_error($con);
+			}
 		
 	//Insert to database if no exist email	
 	}elseif(mysqli_num_rows($run_user_data) == 0) {
